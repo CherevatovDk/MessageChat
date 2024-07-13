2c1f66f939541a04a4bcf1df19c924785407ecc2
Коля Череватов <nounem@outlook.com>
2024-07-13 19:51:20 +0300

    all tasks done and tests for endpoints

--- MessageChat/Program.cs
+++ MessageChat/Program.cs
@@ -1,6 +1,36 @@
+using Core.Interface;
+using Infrastructure.Data;
+using Infrastructure.Repositories;
+using MessageChat;
+using MessageChat.Mapping;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
+        b => b.MigrationsAssembly("Infrastructure")));
+builder.Services.AddSignalR();
+builder.Services.AddAutoMapper(typeof(MappingProfile));
+
+
+
+builder.Services.AddScoped<IChatRepository, ChatRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IChatService, ChatService>();
+builder.Services.AddScoped<IUserService, UserService>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
 
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.MapHub<ChatHub>("/chathub");
 app.Run();
